cc87becf4f6e48f6c34aa579a9b080502fb35987
NIKITA-NISHU <95540333+NIKITA-NISHU@users.noreply.github.com>
2022-12-06 12:12:51 +0530

    Add files via upload

---
+++ Reversestrings.java
@@ -0,0 +1,16 @@
+package reversestring;
+
+public class Reversestrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			 
+			    String input = "mynameisnikita";
+			 
+			    char[] hello = input.toCharArray();
+			    for (int i = hello.length - 1; i >= 0; i--) {
+			      System.out.print(hello[i]);
+	   }
+	}
+}
+
